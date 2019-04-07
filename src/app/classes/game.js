1dbe46e2ad72a972dde9cc4045c194a851ec2bd3
gusadolfo123 <gusadolfo123@hotmail.com>
2019-04-07 18:57:04 -0500

    Ajustes

--- src/app/classes/game.js
+++ src/app/classes/game.js
@@ -1,7 +1,9 @@
-import Position from "./position";
+import Position from './position';
+import {
+  isNullOrUndefined
+} from 'util';
 
 class Game {
-
   constructor(size = 15, canvas) {
     this.food = new Position(0, 0);
     this.score = 0;
@@ -11,7 +13,20 @@ class Game {
     this.ctx = canvas.getContext('2d');
     this.director = null;
     this.direction = 2;
-    this.velocity = 80;
+    this.velocity = 50;
+    this.a = new AudioContext(); // browsers limit the number of concurrent audio contexts, so you better re-use'em
+  }
+
+  beep(vol, freq, duration) {
+    var v = this.a.createOscillator();
+    var u = this.a.createGain();
+    v.connect(u);
+    v.frequency.value = freq;
+    v.type = 'square';
+    u.connect(this.a.destination);
+    u.gain.value = vol * 0.01;
+    v.start(this.a.currentTime);
+    v.stop(this.a.currentTime + duration * 0.001);
   }
 
   generateFood() {
@@ -27,19 +42,44 @@ class Game {
     return number;
   }
 
-  collision() {}
+  collision() {
+    // valida colision contra paredes
+    if (this.snake[0].Y == this.canvas.height) {
+      console.log(this.snake[0].Y, this.snake[0].X);
+      console.log('Colisiono 1');
+    } else if (this.snake[0].Y == -this.size) {
+      console.log(this.snake[0].Y, this.snake[0].X);
+      console.log('Colisiono 2');
+    } else if (this.snake[0].X == this.canvas.width) {
+      console.log(this.snake[0].X, this.snake[0].Y);
+      console.log('Colisiono 3');
+    } else if (this.snake[0].X == -this.size) {
+      console.log(this.snake[0].X, this.snake[0].Y);
+      console.log('Colisiono 4');
+    }
+
+    // valida colision contra si mismo
+    for (let index = 1; index < this.snake.length; index++) {
+      const element = this.snake[index];
+      if (this.snake[0].X == element.X && this.snake[0].Y == element.Y) {
+        console.log('Colisiono contra si mismo');
+      }
+    }
+  }
 
   init() {
     this.generateFood();
     this.snake.push(new Position(this.size, this.size));
     this.director = setInterval(() => {
+      // this.next();
+      //this.nextU(this.snake.length - 1);
       this.next();
+      this.collision();
       this.show();
     }, this.velocity);
   }
 
   show() {
-
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
     this.snake.map(element => {
@@ -47,14 +87,13 @@ class Game {
       this.ctx.fillRect(element.X, element.Y, this.size, this.size);
     });
 
+
     this.ctx.fillStyle = 'red';
     this.ctx.fillRect(this.food.X, this.food.Y, this.size, this.size);
-
   }
 
   next() {
     for (let index = 0; index < this.snake.length; index++) {
-
       let positionPrev = Object.assign(new Position(), this.snake[index]);
 
       switch (this.direction) {
@@ -75,12 +114,32 @@ class Game {
       if (this.snake[0].X == this.food.X && this.snake[0].Y == this.food.Y) {
         this.score += 10;
         this.snake.push(positionPrev);
+        this.beep(100, 520, 200);
         this.generateFood();
       }
       break;
     }
   }
 
+  nextU(index) {
+    console.log(index, this.snake);
+    if (isNullOrUndefined(this.snake[index])) return 0;
+    switch (this.direction) {
+      case 1:
+        this.snake[index].Y -= this.size;
+        break;
+      case 2:
+        this.snake[index].Y += this.size;
+        break;
+      case 3:
+        this.snake[index].X += this.size;
+        break;
+      case 4:
+        this.snake[index].X -= this.size;
+        break;
+    }
+    return this.nextU(index++);
+  }
 }
 
 export default Game;
